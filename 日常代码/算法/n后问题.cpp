6b893d836cb96138ca6bd09cf15d2b31baf12eee
shengshengxiaowang <278434650@qq.com>
2019-05-23 01:51:47 +0800

    n后问题

--- 日常代码/算法/n后问题.cpp
+++ 日常代码/算法/n后问题.cpp
@@ -3,6 +3,7 @@
 //由于n个数不在同一行也不在同一列，共n个皇后，所以每行都会有一个皇后
 //并且这个皇后所在代表列的数是唯一不重复的，这就相当于给n个数全排列，然后判断排列的数在每列上有没有重复即可.
 //套用全排列回溯递归即可
+
 #include<iostream>
 #include<math.h>
 using namespace std;
@@ -33,6 +34,11 @@ void fun(int *a,int k)//k是行
     if(k>n)
     {
         //cout<<a<<endl;
+        for(int i=1;i<=n;i++)
+        {
+            cout<<a[i];
+        }
+        cout<<endl<<k<<endl;
         count++;
         return;
     }
@@ -40,19 +46,13 @@ void fun(int *a,int k)//k是行
     {
         for(int i=k;i<=n;i++)
         {
-            swap(&a[k],&a[i]);//交换，传值地址进去
+            swap(&a[k],&a[i]);//交换，传地址进去
 
             if(k==1 || panduan(a,k))
             {
                 fun(a,k+1); //这块儿要注意不能用k++,因为下面还要继续用k
             }
             swap(&a[k],&a[i]);
-
-            /*for(int i=1;i<=n;i++)
-            {
-                cout<<a[i];
-            }
-            cout<<endl<<k<<endl;*/
         }
     }
 
