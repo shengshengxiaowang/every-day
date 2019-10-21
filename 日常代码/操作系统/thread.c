ef42552e2d1b56e348e1644ecb57d5dba8ca0c8d
shengshengxiaowang <278434650@qq.com>
2019-10-21 11:46:06 +0800

    thread

--- 日常代码/操作系统/thread.c
+++ 日常代码/操作系统/thread.c
@@ -9,10 +9,10 @@
 unsigned long long main_counter,counter[MAX_THREAD];
 //main_counter是主线程，counter[]各个线程不断加1
 
-void* thread_worker(void* p)
+void* thread_worker(void *p)
 {
     int thread_num;
-    thread_num=*((int *)p);  //强转 让thread_num=i
+    thread_num=(int)p;  //强转 让thread_num=i  c++语法不支持
     for(;;)   //无限循环
     {
         counter[thread_num]++;
@@ -26,9 +26,9 @@ int main(int argc,char* argv[])
     pthread_t pthread_id[MAX_THREAD]={0}; //存放线程id
     for(i=0;i<MAX_THREAD;i++)
     {
-        //用pthread_create创建普通线程，线程id存入数组
+        //用pthread_create创建3个普通线程，线程id存入数组
         //线程执行函数是thread_worker,将i作为参数传入
-        rth=pthread_create(&pthread_id[i] ,NULL,thread_worker,&i);
+        rth=pthread_create(&pthread_id[i] ,NULL,thread_worker,(void*)i); //question问题，如果传进去的是&i，则会出现线程为0的情况
         //第一个线程为指向线程标识符的指针，第二个设置线程属性，第三个为线程函数，第四个为函数参数
     }
     do  //用户按一次回车执行循环一次,q退出
@@ -46,7 +46,7 @@ int main(int argc,char* argv[])
 
 
 /*
-1.前三列不一定相等,斜杠两边，后面的数字大于等于前面数字，
+1.前三列不一定相等,斜杠两边，不一定，可能最开始的时候后面的比前面的大，因为sum执行较慢，所以会执行sum的时候还是之前的值
   因为前面的main_counter是主线程，即每次运行一个线程都会+1,而sum是所有线程和，和肯定包括了每次运行都+1的那个线程的标识符，所有肯定大于等于
 2.结果不如我所料，最开始我以为前面三个线程数是不一样的，但是最后发现是随机的
   没有分析出原因
