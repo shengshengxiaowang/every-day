a29b8d03e74f0001d7d0036994a0d302a73f6c9c
shengshengxiaowang <278434650@qq.com>
2019-11-09 22:17:57 +0800

    ff

--- 日常代码/操作系统/ff.c
+++ 日常代码/操作系统/ff.c
@@ -35,7 +35,7 @@ int free_block_count=0; //空闲块的个数初始为0
 int mem_size=default_mem_size; //内存大小
 int free_mem_size=0;   //当前空闲内存大小
 int algorithm=ma_ff;   //当前分配算法，默认ff
-static int pid=0;
+static int pid=0;       //进程号，即第几个进程
 int flag=0;     //标志内存是否已经被设置，如果设置过，则不能再设置
 
 struct free_block* init_free_block(int mem_size);//初始化空闲内存块头指针
@@ -43,9 +43,10 @@ void menu();//显示菜单
 int set_mem_size(); //设置内存大小
 void set_algorithm();//设置分配算法
 void rearrange(int a);  //算法跳转函数，跳转到对应算法的函数中去
-int rearrange_ff(); //ff算法函数
-
-
+int rearrange_ff(); //ff算法初始化函数
+int new_process();  //创建新进程函数
+int allocated_mem(struct allocated_block *ab);  //内存分配算法函数
+int allocated_ff(struct allocated_block *ab);  //内存分配ff算法
 
 int set_mem_size() //设置内存大小,默认为之前设定的值为1024
 {
@@ -100,6 +101,69 @@ struct free_block* init_free_block(int mem_size)
     return fb;
 }
 
+int allocated_ff(struct allocated_block *ab)  //内存分配ff算法
+{
+    int ret=0;
+    struct free_block *pre=NULL,*ff=free_block_head;
+    if(ff==NULL)  //ff指向空闲块头指针
+    {
+        return -1;
+    }
+    while(ff!=NULL)  //开始遍历链表
+    {
+        if(ff->size>=ab->size)  //遇到的第一个大小合适，则分配
+        {
+            ret=allocate(pre,ff)
+        }
+    }
+}
+
+int allocated_mem(struct allocated_block *ab)  //内存分配算法函数
+{
+    int ret=0; //分配是否成功标识
+    struct free_block *fbt,*pre;
+    int request_size=ab->size;
+    fbt=pre=free_block_head; //指向空闲块头指针
+    switch(algorithm)  
+    {
+        case ma_ff:
+            ret=allocated_ff(ab);
+            break;
+    }
+    return ret;
+}
+
+int new_process()  //创建新进程函数
+{
+    struct allocated_block *ab; //新空间结构体
+    int ret;  //内存分配返回标识
+    ab=(struct allocated_block*)malloc(sizeof(struct allocated_block));
+    if(!ab)  //如果申请错误
+    {
+        exit(-5);
+    }
+    ab->next=NULL;
+    printf("请输入内存大小:\n");
+    scanf("%d",&ab->size);  //输入大小
+    getchar();  //吸收回车字符
+
+    ab->pid=pid+1;  //数据填入结构体
+    sprintf(ab->process_name,"process-02%d",ab->pid);
+    if(ab->size<=0)  //如果输入负数，返回
+    {
+        printf("输入错误，返回\n");
+        free(ab);  //释放ab空间
+        return -1;
+    }
+    //如果内存大小正确，则分配算法
+    ret=allocated_mem(ab);  
+    
+
+
+    pid++;  //进程号 ++
+}
+
+
 int rearrange_ff()  //ff算法函数
 {
     struct free_block *head=free_block_head; //指向空闲链表头指针
@@ -119,12 +183,31 @@ int rearrange_ff()  //ff算法函数
             //比较第一个跟第二个空闲块的开始位置大小
             if(forehand==head&& forehand->start_addr>=pre->start_addr)
             {
-                head->next=pre->next;                                                                                                                                                                                                                                                   
+                //如果是第一个结点起始位置大于第一个结点的起始位置，交换
+                head->next=pre->next;//head的下一个指向第三个    
+                pre->next=head;  //第二个的
+                head=pre;
+                forehand=head->next;
+                pre=forehand->next;
+                rear=pre->next;
+            }
+            else if(pre->start_addr >=rear->start_addr)
+            {//比较其他的，如果前一个大于后一个
+                pre->next=rear->next;//前一个的下一个等于后一个的下一个
+                forehand->next=rear;//前一个的前一个的下一个指向后一个
+                rear->next=pre;//后一个的下一个指向前一个
+                forehand=rear;  //指针后移
+                rear=pre->next;  //指针后移
+            }
+            else
+            {
+                forehand=pre;  //指针后移
+                pre=rear;
+                rear=rear->next;
             }
         }
-
-
     }
+    return 0;
 }
 
 
@@ -157,15 +240,17 @@ void set_algorithm()
     {
         printf("错误，返回上一层\n");
     }
-    return;
-    
+    return;    
 }
+
 int main()
 {
     free_block_head=init_free_block(mem_size); //初始化空闲块的头指针，mem_size为之前设定的大小，函数返回值为头结点
     char choice;   
     pid =0;
-    while(1)        menu();
+    while(1)        
+    {
+        menu();
         choice=getchar();
         getchar();
         switch(choice)
@@ -178,6 +263,10 @@ int main()
                 flag=1;
                 break;
             case '3':
+                new_process();   //创建新进程  
+                flag=1;
+                break;
+            //case '4':
         }
     }
 }
